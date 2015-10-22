6a9829070638080d76c62296b0648b52b7f46d86
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-10-22 15:58:38 -0400

    Watch me crank that Rubocop!

--- packyao-build.rb
+++ packyao-build.rb
@@ -1,19 +1,19 @@
 require 'pp'
 
 def generate_script
-  require "erb"
-  require "securerandom"
+  require 'erb'
+  require 'securerandom'
   filename = "run-#{SecureRandom.uuid}.sh"
 
-  template = IO.read("run.sh.erb")
-  message = ERB.new(template, 0, "%<>")
+  template = IO.read('run.sh.erb')
+  message = ERB.new(template, 0, '%<>')
   File.write(filename, message.result(binding))
-  return filename
+  filename
 end
 
 def generate_dockerfile(filename)
-  template = IO.read("Dockerfile.erb")
-  message = ERB.new(template, 0, "%<>")
+  template = IO.read('Dockerfile.erb')
+  message = ERB.new(template, 0, '%<>')
   File.write('Dockerfile', message.result(binding))
 end
 
@@ -22,10 +22,10 @@ generate_dockerfile(filename)
 exit
 
 require 'docker'
-puts "Creating image..."
+puts 'Creating image...'
 image = Docker::Image.build_from_dir('.')
 pp image.json
-puts "Creating container..."
+puts 'Creating container...'
 container = Docker::Container.create('Image' => image.id)
 pp container.json
 container.start
