a63c80cbd895b2ec7b0b0c99e56366c57a0c9c5b
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-10-22 12:32:44 -0400

    Use ERB to generate Dockerfile.

--- packyao-build.rb
+++ packyao-build.rb
@@ -1,6 +1,16 @@
-require 'docker'
 require 'pp'
+def generate_dockerfile
+  require "erb"
 
+  template = IO.read("Dockerfile.erb")
+  message = ERB.new(template, 0, "%<>")
+  File.write('Dockerfile', message.result)
+end
+
+generate_dockerfile
+exit
+
+require 'docker'
 puts "Creating image..."
 image = Docker::Image.build_from_dir('.')
 pp image.json
@@ -8,4 +18,3 @@ puts "Creating container..."
 container = Docker::Container.create('Image' => image.id)
 pp container.json
 container.start
-
